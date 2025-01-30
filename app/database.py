64824f56ff698adadfde81017e3ccb609a2669fb
zumaku <60200121101@uin-alauddin.ac.id>
2025-01-30 13:23:08 +0800

    Update

--- app/database.py
+++ app/database.py
@@ -5,18 +5,28 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 from databases import Database
 
+# Load environment variables
 load_dotenv()
 
+# Ambil dari environment
 DB_USERNAME = os.getenv('DB_USERNAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_URL = os.getenv('DB_URL')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_URL}/{DB_NAME}"
+# Pastikan tidak ada None value
+if None in [DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME]:
+    raise ValueError("⚠️ ERROR: Beberapa environment variables tidak termuat dengan benar!")
 
+# Buat URL database
+DATABASE_URL = f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Inisialisasi database
 database = Database(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
 metadata.create_all(bind=engine)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
